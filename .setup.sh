02b72a373d1a5a3aed06878ccf9df06ea1c25332
David Jaress <davidjaress@Davids-MBP.hsd1.il.comcast.net>
2021-01-25 15:58:49 -0600

    Update setup, config

--- .setup.sh
+++ .setup.sh
@@ -1,11 +1,2 @@
-# Install zshell
-sudo apt-get install zsh
-
-# Install oh-my-zshell
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
-# Fix PATH reference for RVM per https://github.com/rvm/rvm/issues/3212
-sed -i 's/export PATH=/export PATH=$PATH:/g' ~/.zshrc
-
 # Source config
-echo "source ~/zsh_config/.zsh_aliases" >> ~/zsh_config/.zsh_aliases
+echo "[ -f ~/apps/github/zensaburou/zsh_config/.zsh_aliases] && source ~/apps/github/zensaburou/zsh_config/.zsh_aliases" > ~/.zshrc
